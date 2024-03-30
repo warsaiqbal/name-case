3e551ec66250dac0b7296710839c366e67fb4687
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 01:43:07 -0700

    Add files via upload

---
+++ 3. Famous quote/index.js
@@ -0,0 +1,2 @@
+var quote = "Quaid-e-azam once said, 'Success consists in moving from failure to failure without loss of enthusiasm'.";
+console.log(quote);
---
+++ 3. Famous quote/index.ts
@@ -0,0 +1,2 @@
+let quote: string = "Quaid-e-azam once said, 'Success consists in moving from failure to failure without loss of enthusiasm'."
+console.log(quote);
